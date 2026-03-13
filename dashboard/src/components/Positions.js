6b45c398523f9200faae78e39a459f52f4ddc7ed
kshitiz <horizonnn2003@gmail.com>
2026-03-13 08:25:38 +0530

    in this commit we have successfully achieve dynamic nature in holdingd, positions, watchli st by our custon data

--- dashboard/src/components/Positions.js
+++ dashboard/src/components/Positions.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import { positions } from "../data/data";
 const Positions = () => {
   return (
     <>
-      <h3 className="title">Positions (2)</h3>
+      <h3 className="title">Positions ({positions.length})</h3>
 
       <div className="order-table">
         <table>
@@ -16,6 +16,24 @@ const Positions = () => {
             <th>P&L</th>
             <th>Chg.</th>
           </tr>
+
+          {positions.map((stock, index) => {
+            const currValue = stock.price * stock.qty;
+            const isProfit = currValue - stock.avg * stock.qty >= 0.0;
+            const profClass = isProfit ? "profit" : "loss";
+
+            return (
+              <tr key={index}>
+                <td>{stock.product}</td>
+                <td>{stock.name}</td>
+                <td>{stock.qty}</td>
+                <td>{stock.avg.toFixed(2)}</td>
+                <td>{stock.price.toFixed(2)}</td>
+                <td className={profClass}>{(currValue-stock.avg * stock.qty).toFixed(2)}</td>
+                <td className={profClass}>{stock.net}</td>
+              </tr>
+            );
+          })}
         </table>
       </div>
     </>
--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -1,5 +1,10 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Tooltip, Grow } from "@mui/material";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';   
+import MoreHorizIcon  from '@mui/icons-material/MoreHoriz';   
+import { watchlist } from "../data/data";
 const WatchList = () => {
   return (
     <div className="watchlist-container">
@@ -11,12 +16,62 @@ const WatchList = () => {
           placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
           className="search"
         />
-        <span className="counts"> 9 / 50</span>
+        <span className="counts">{watchlist.length}/50</span>
       </div>
 
-      <ul className="list"></ul>
+      <ul className="list">
+        {watchlist.map((stock, index) => {
+          return <WatchListItem stock={stock} key={index} />;
+        })}
+      </ul>
     </div>
   );
 };
 
 export default WatchList;
+
+const WatchListItem = ({ stock, key }) => {
+  const [showWatchListActions, setshowWatchListActions] = useState(false);
+
+  const handleMouseEnter = () => {
+    setshowWatchListActions(true);
+  };
+  const handleMouseExit = () => {
+    setshowWatchListActions(false);
+  };
+
+  return (
+    <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseExit}>
+      <div className="item">
+        <p className={stock.isDown ? "down" : "up"}>{stock.name}</p>
+        <div className="itemInfo">
+          <span className="percent">{stock.percent}</span>
+          {stock.isDown ? (
+            <KeyboardArrowDownIcon className="down" />
+          ) : (
+            <KeyboardArrowUpIcon className="up" />
+          )}
+          <span className="price">{stock.price}</span>
+        </div>
+      </div>
+      {showWatchListActions && <WatchListActions uid={stock.name}/>}
+    </li>
+  );
+};
+
+const WatchListActions = ({uid}) => {
+  return (
+    <span className="actions">
+      <span className="actions">
+        <Tooltip title="Buy (b)" placement="top" arrow TransitionComponent={Grow}><button className="buy">Buy</button></Tooltip>
+         <Tooltip title="Sell (s)" placement="top" arrow TransitionComponent={Grow}><button className="sell">Sell</button></Tooltip>
+          <Tooltip title="Analytics (a)" placement="top" arrow TransitionComponent={Grow}>
+            <button className="action"><BarChartOutlinedIcon className="icon"/></button>
+          </Tooltip>
+           <Tooltip title="More (m)" placement="top" arrow TransitionComponent={Grow}>
+            <button className="action"><MoreHorizIcon className="icon"/></button>
+          </Tooltip>
+      </span>
+    </span>
+  )
+}
