c23394ba092556a2580ee28530934d839430c256
kshitiz <horizonnn2003@gmail.com>
2026-03-14 06:50:39 +0530

    in this commit we have solve api related problem

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,39 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const BodyParser = require("body-parser")
+const cors = require("cors")
 const PORT = process.env.PORT || 3002;
 const MONGO_URL = process.env.MONGO_URL;
 const mongoose = require("mongoose");
+const { HoldingModel } = require("./model/HoldingModel");
+const { PositionModel } = require("./model/PositionModel");
+const bodyParser = require("body-parser");
 
-async function main(){
+app.use(cors());
+app.use(bodyParser.json());
+
+async function main() {
   await mongoose.connect(MONGO_URL);
-};
+}
 
 main()
-.then(()=>{
-  console.log("connected to Db")
-})
-.catch((err)=>{
-  console.log(err);
-})
+  .then(() => {
+    console.log("connected to Db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/allHoldings", async (req, res) => {
+  let allHoldings = await HoldingModel.find({});
+  res.json(allHoldings);
+});
+app.get("/allPositions", async (req, res) => {
+  let allPositions = await PositionModel.find({});
+  res.json(allPositions);
+});
+
 app.listen(PORT, () => {
   console.log(`server is listening to port ${PORT}`);
 });
