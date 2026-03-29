23e789d48d6202e39c34bb78bcbaeeb76a5a8d3a
kshitiz <horizonnn2003@gmail.com>
2026-03-30 02:52:48 +0530

    in this commit we have added buy and sell window using general context

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const BodyParser = require("body-parser")
-const cors = require("cors")
+const BodyParser = require("body-parser");
+const cors = require("cors");
 const PORT = process.env.PORT || 3002;
 const MONGO_URL = process.env.MONGO_URL;
 const mongoose = require("mongoose");
 const { HoldingModel } = require("./model/HoldingModel");
 const { PositionModel } = require("./model/PositionModel");
+const { OrderModel } = require("./model/OrderModel");
 const bodyParser = require("body-parser");
 
 app.use(cors());
@@ -33,7 +34,20 @@ app.get("/allPositions", async (req, res) => {
   let allPositions = await PositionModel.find({});
   res.json(allPositions);
 });
-
+app.post("/newOrder", async (req, res) => {
+  let newOrder = await new OrderModel({
+    name: req.body.name,
+    qty: req.body.qty,
+    price: req.body.price,
+    mode: req.body.mode,
+  });
+  newOrder.save();
+  res.send("done")
+});
+app.get("/allOrders",async (req,res)=>{
+  let allOrders = await OrderModel.find({});
+  res.json(allOrders);
+})
 app.listen(PORT, () => {
   console.log(`server is listening to port ${PORT}`);
 });
