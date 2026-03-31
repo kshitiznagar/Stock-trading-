2b0c5c2f2b540f5a82165b70a3334a4136baea3e
kshitiz <horizonnn2003@gmail.com>
2026-03-31 22:16:47 +0530

    in this commit we have successfully implemented backend of  Authorization using JWT

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,14 @@ const mongoose = require("mongoose");
 const { HoldingModel } = require("./model/HoldingModel");
 const { PositionModel } = require("./model/PositionModel");
 const { OrderModel } = require("./model/OrderModel");
+const { UserModel } = require("./model/UserModel");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.json());
 
 async function main() {
   await mongoose.connect(MONGO_URL);
@@ -42,12 +46,53 @@ app.post("/newOrder", async (req, res) => {
     mode: req.body.mode,
   });
   newOrder.save();
-  res.send("done")
+  res.send("done");
 });
-app.get("/allOrders",async (req,res)=>{
+app.get("/allOrders", async (req, res) => {
   let allOrders = await OrderModel.find({});
   res.json(allOrders);
-})
+});
+//Register a user
+app.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    console.log(req.body);
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser)
+      return res.status(400).json({ message: "User already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new UserModel({ username, email, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({message : "Server error"});
+  }
+});
+//login and issue JWT
+app.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await UserModel.findOne({ email });
+    if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+
+    const payload = { id: user._id, email: user.email };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '15m' });
+
+    res.json({ token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
 app.listen(PORT, () => {
   console.log(`server is listening to port ${PORT}`);
 });
